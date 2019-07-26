3bb997dea8261c7752a48f0d865dcf23c91f5be7
Randy Wilson <phoenixrr2113@gmail.com>
2019-07-26 12:12:54 -0400

    Updates

---
+++ precache-manifest.af74532ae69db295ff94e98188312046.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "736a8257bf6dfccd00fe0bce4e1bbf08",
+    "url": "/porcelain/index.html"
+  },
+  {
+    "revision": "f52bd18153d47f5b4763",
+    "url": "/porcelain/static/css/2.d14e8350.chunk.css"
+  },
+  {
+    "revision": "9c32bd070b28d388bfe2",
+    "url": "/porcelain/static/css/main.38d0b56d.chunk.css"
+  },
+  {
+    "revision": "f52bd18153d47f5b4763",
+    "url": "/porcelain/static/js/2.8fbe4b64.chunk.js"
+  },
+  {
+    "revision": "9c32bd070b28d388bfe2",
+    "url": "/porcelain/static/js/main.1e832b08.chunk.js"
+  },
+  {
+    "revision": "1a75f79ce0f80f56188e",
+    "url": "/porcelain/static/js/runtime~main.d44381ee.js"
+  },
+  {
+    "revision": "4fe6f9caff8b287170d51d3d71d5e5c6",
+    "url": "/porcelain/static/media/lg.4fe6f9ca.ttf"
+  },
+  {
+    "revision": "5fd4c338c1a1b1eeeb2c7b0a0967773d",
+    "url": "/porcelain/static/media/lg.5fd4c338.woff"
+  },
+  {
+    "revision": "c066c5448562b3ccaefb6408ce4b4ae1",
+    "url": "/porcelain/static/media/lg.c066c544.svg"
+  },
+  {
+    "revision": "ecff11700aad0000cf3503f537d1df17",
+    "url": "/porcelain/static/media/lg.ecff1170.eot"
+  }
+]);
